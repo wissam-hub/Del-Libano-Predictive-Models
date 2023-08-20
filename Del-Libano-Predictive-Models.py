3f12067a59cbb0d4d7ccc32a8e0007972aec72a0
wissam-hub <63789163+wissam-hub@users.noreply.github.com>
2023-08-20 19:45:54 +0300

    Update Del-Libano-Predictive-Models.py

--- Del-Libano-Predictive-Models.py
+++ Del-Libano-Predictive-Models.py
@@ -307,6 +307,19 @@ def forecast_and_visualize(X, model, selected_product, feature_engineered_data):
     ax.set_ylabel('Production')
     st.pyplot(fig)
 
+    # # Create a table with the forecasted values
+    # forecast_table = pd.DataFrame({'Date': future_dates, 'Forecast': y_forecast})
+    
+    # # Format the Date column as yyyy-mm-dd
+    # forecast_table['Date'] = forecast_table['Date'].dt.strftime('%Y-%m-%d')
+    
+    # # Round the Forecast column to 5 decimal places using the apply method
+    # forecast_table['Forecast'] = forecast_table['Forecast'].apply(lambda x: round(x, 5))
+
+    # st.write(forecast_table)
+
+
+# horizontally 
     # Create a table with the forecasted values
     forecast_table = pd.DataFrame({'Date': future_dates, 'Forecast': y_forecast})
     
@@ -315,22 +328,9 @@ def forecast_and_visualize(X, model, selected_product, feature_engineered_data):
     
     # Round the Forecast column to 5 decimal places using the apply method
     forecast_table['Forecast'] = forecast_table['Forecast'].apply(lambda x: round(x, 5))
-
-    st.write(forecast_table)
-
-
-# # horizontally 
-#     # Create a table with the forecasted values
-#     forecast_table = pd.DataFrame({'Date': future_dates, 'Forecast': y_forecast})
     
-#     # Format the Date column as yyyy-mm-dd
-#     forecast_table['Date'] = forecast_table['Date'].dt.strftime('%Y-%m-%d')
-    
-#     # Round the Forecast column to 5 decimal places using the apply method
-#     forecast_table['Forecast'] = forecast_table['Forecast'].apply(lambda x: round(x, 5))
-    
-#     # Display the table horizontally
-#     st.write(forecast_table.set_index('Date').T)
+    # Display the table horizontally
+    st.write(forecast_table.set_index('Date').T)
 
 
 
